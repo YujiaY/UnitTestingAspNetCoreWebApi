4ea7c4b9a462460ab4d3f8bad81e2bf80380e969
Jack Yuan <jack.yujia.yuan@gmail.com>
2024-06-07 21:30:33 +1000

    Data Source=:memory:

---
+++ Starter files/EmployeeManagement.Test/TestIsolationApproachesTests.cs
@@ -0,0 +1,60 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.DbContexts;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
+using FluentAssertions;
+// using EmployeeManagement.Test.HttpMessageHandlers;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Xunit.Sdk;
+
+namespace EmployeeManagement.Test
+{
+    public class TestIsolationApproachesTests
+    {
+        [Fact]
+        public async Task AttendCourseAsync_CourseAttended_SuggestedBonusMustCorrectlyBeRecalculated()
+        {
+            // Arrange
+            SqliteConnection connection = new("Data Source=:memory:");
+            connection.Open();
+
+            DbContextOptionsBuilder<EmployeeDbContext> optionsBuilder = 
+                new DbContextOptionsBuilder<EmployeeDbContext>().UseSqlite(connection);
+
+            EmployeeDbContext dbContext = new(optionsBuilder.Options);
+            await dbContext.Database.MigrateAsync();
+
+            EmployeeManagementRepository employeeManagementDataRepository = new(dbContext);
+
+            EmployeeService employeeService = new(
+                employeeManagementDataRepository,
+                new EmployeeFactory());
+
+            // get course from database - "Dealing with Customers 101"
+            Course? courseToAttend = await employeeManagementDataRepository
+                .GetCourseAsync(Guid.Parse("844e14ce-c055-49e9-9610-855669c9859b"));
+
+            // get existing employee - "Megan Jones"
+            InternalEmployee? internalEmployee = await employeeManagementDataRepository
+                .GetInternalEmployeeAsync(Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+            if (courseToAttend == null || internalEmployee == null)
+            {
+                throw new XunitException("Arranging the test failed");
+            }
+
+            // expected suggested bonus after attending the course
+            var expectedSuggestedBonus = internalEmployee.YearsInService
+                * (internalEmployee.AttendedCourses.Count + 1) * 100;
+
+            // Act
+            await employeeService.AttendCourseAsync(internalEmployee, courseToAttend);
+
+            // Assert
+            Assert.Equal(expectedSuggestedBonus, internalEmployee.SuggestedBonus);
+            
+            internalEmployee.SuggestedBonus.Should().Be(expectedSuggestedBonus);
+        }
+    }
+}
