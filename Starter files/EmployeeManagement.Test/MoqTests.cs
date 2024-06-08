14375287685703c21c0c0358ce32629a3acb3f3b
Jack Yuan <jack.yujia.yuan@gmail.com>
2024-06-08 20:19:05 +1000

    Initially use of Moq.

---
+++ Starter files/EmployeeManagement.Test/MoqTests.cs
@@ -0,0 +1,84 @@
+﻿using EmployeeManagement.Business;
+using EmployeeManagement.DataAccess.Entities;
+using EmployeeManagement.DataAccess.Services;
+using EmployeeManagement.Services.Test;
+using FluentAssertions;
+using Moq;
+using Xunit;
+
+namespace EmployeeManagement.Test
+{
+    public class MoqTests
+    {
+        [Fact]
+        public void FetchInternalEmployee_EmployeeFetched_SuggestedBonusMustBeCalculated()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository =
+              new EmployeeManagementTestDataRepository();
+            var employeeFactoryOrigin = new EmployeeFactory();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                // employeeFactoryOrigin);
+                employeeFactoryMock.Object);
+
+            // Act 
+            var employee = employeeService.FetchInternalEmployee(
+                Guid.Parse("72f2f5fe-e50c-4966-8420-d50258aefdcb"));
+
+            // Assert  
+            Assert.Equal(400, employee.SuggestedBonus);
+
+            employee.SuggestedBonus.Should().Be(400);
+        }
+        
+        [Fact]
+        public void CreateInternalEmployee_InternalEmployeeCreated_SuggestedBonusMustBeCalculated()
+        {
+            // Arrange
+            var employeeManagementTestDataRepository =
+                new EmployeeManagementTestDataRepository();
+            var employeeFactoryMock = new Mock<EmployeeFactory>();
+            employeeFactoryMock.Setup(m =>
+                    m.CreateEmployee(
+                        "Kevin",
+                        It.IsAny<string>(),
+                        null,
+                        false))
+                .Returns(new InternalEmployee(
+                    "Kevin", "Dockx", 5, 2500, false, 1));
+            
+            employeeFactoryMock.Setup(m =>
+                    m.CreateEmployee(
+                        "Sandy",
+                        It.IsAny<string>(),
+                        null,
+                        false))
+                .Returns(new InternalEmployee("Sandy", "Dockx", 5, 3000, false, 1));
+            
+            employeeFactoryMock.Setup(m =>
+                    m.CreateEmployee(
+                        It.Is<string>(value => value.Contains("a")),
+                        It.IsAny<string>(),
+                        null,
+                        false))
+                .Returns(new InternalEmployee("SomeoneWithAna", "Dockx", 5, 3000, false, 1));
+
+            var employeeService = new EmployeeService(
+                employeeManagementTestDataRepository,
+                employeeFactoryMock.Object);
+
+            // suggested bonus for new employees =
+            // (years in service if > 0) * attended courses * 100  
+            decimal suggestedBonus = 1000;
+
+            // Act 
+            var employee = employeeService.CreateInternalEmployee("Sandy", "Dockx");
+
+            // Assert  
+            Assert.Equal(suggestedBonus, employee.SuggestedBonus);
+        }
+
+    }
+}
